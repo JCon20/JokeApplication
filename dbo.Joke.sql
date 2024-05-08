2dda156ea1b59c093d3ecaebda82cbaae799e67e
JCon20 <46573361+JCon20@users.noreply.github.com>
2024-05-08 09:08:18 -0500

    Add project files.

---
+++ dbo.Joke.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Joke] (
+    [ID]           INT            IDENTITY (1, 1) NOT NULL,
+    [JokeQuestion] NVARCHAR (MAX) NULL,
+    [JokeAnswer]   NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_Joke] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
